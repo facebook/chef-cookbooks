a7efc3e094c7aad9e2f39f50610b670f2112302b
Phil Dibowitz <phil@ipom.com>
2016-04-15 16:15:08 -0700

    More license
    Reviewed By: davide125
    
    Differential Revision: D3187624
    
    fb-gh-sync-id: 3ad208ee4dda079d722fc0c3a5730166c93374e2
    fbshipit-source-id: 3ad208ee4dda079d722fc0c3a5730166c93374e2

--- fb_fstab/recipes/default.rb
+++ fb_fstab/recipes/default.rb
@@ -2,7 +2,12 @@
 # Cookbook Name:: fb_fstab
 # Recipe:: default
 #
-# Copyright 2013-present, Facebook
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant 
+# of patent rights can be found in the PATENTS file in the same directory.
 #
 # vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
 #
--- fb_modprobe/providers/module.rb
+++ fb_modprobe/providers/module.rb
@@ -1,4 +1,11 @@
 # vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant 
+# of patent rights can be found in the PATENTS file in the same directory.
+#
 def modprobe_module(module_name, params, timeout, verbose, unload)
   verbose_flags = verbose ? '-v ' : ''
   remove_flags = unload ? '-r ' : ''
