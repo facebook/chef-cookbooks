ff5c53f2cade7bf74c28453e673f8884e0627702
Davide Cavalca <dcavalca@fb.com>
2016-05-01 10:39:23 -0700

    add fb_p910nd cookbook

---
+++ fb_p910nd/attributes/default.rb
@@ -0,0 +1,16 @@
+# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+
+default['fb_p910nd'] = {
+  'number' => 0,
+  'device' => '/dev/usb/lp0',
+  'bidirectional' => true,
+  'listen' => '',
+}
