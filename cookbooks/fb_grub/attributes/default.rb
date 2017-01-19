e5424f784f9e798439d89e6fb71db2758cb4d642
Marc Seeger <mseeger@fb.com>
2017-01-19 14:23:53 -0800

    Add support for grub 1 device hints and matching unit tests
    Summary:
    While we currently extract the root drive from an existing grub configuration, we don't extract any of the "device" statements yet.
    These should always go in pairs, seeing as the device statement 'remaps' the meaning of the root disk 'hd0,1' string.
    
    Tests:
    Because I've run into issues recently where I missed various corner cases when attempting to extract the root drive, this seems like a good place for a quick sanity check.
    
    Reviewed By: jaymzh
    
    Differential Revision: D4436124
    
    fbshipit-source-id: fa3eb1c69fbfdee03823c6628c659d8d28993a8c

--- cookbooks/fb_grub/attributes/default.rb
+++ cookbooks/fb_grub/attributes/default.rb
@@ -19,6 +19,7 @@ fb_grub = {
   '_grub_base_dir' => '/boot/grub',
   '_grub2_base_dir' => '/boot/grub2',
   '_grub2_module_path' => '/notdefined',
+  '_device_hints' => [],
   '_vendor' => vendor,
   'terminal' => [
     'console',
@@ -73,28 +74,15 @@ end
 # the recipes folder.
 original_grub_config = '/root/grub.before_chef'
 if File.exist?(original_grub_config)
-  # Grub 2 data looks like this:
-  # menuentry 'CentOS (4.0.9-75_fbk14_4148_g37fe86f)' {
-  #   set root='(hd0,1)'
-  # or
-  #   set root='hd0,gpt2'
-  #
-  #
-  # Grub 1 data looks like this:
-  # title CentOS (4.0.9-68_fbk12_4058_gd84fdb3)
-  #   root (hd0,0)
-  #
-  # Extracted string will be: hd0,2 regardless
   content = File.read(original_grub_config)
-  # This will extract the first root directive from the file that is
-  # not a comment.
-  # This isn't a very generalizable solution.
-  filter_regexp = /^\s*[^#].*root[=\s]+['"]?\(?([\w,]+)\)?['"]?/
-  original_root_device = content[filter_regexp, 1]
-  if original_root_device && !original_root_device.empty?
+  original_root_device = FB::Grub.extract_root_device(content)
+  original_device_hints = FB::Grub.extract_device_hints(content)
+  if original_root_device
     # Setting this will make sure we don't
     fb_grub['root_device'] = original_root_device
-    Chef::Log.info("Re-using existing root device: #{original_root_device}")
+    Chef::Log.debug("Re-using existing root device: #{original_root_device}")
+    fb_grub['_device_hints'] = original_device_hints
+    Chef::Log.debug("Found #{original_device_hints.size} grub device hints.")
   else
     fail "fb_grub::default Can't parse grub config: #{original_grub_config}"
   end
---
+++ cookbooks/fb_grub/libraries/default.rb
@@ -0,0 +1,45 @@
+# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+
+module FB
+  # Grub utility functions
+  class Grub
+    # This will extract the first root directive from the file that is
+    # not a comment.
+    # This isn't a very generalizable solution.
+    # Grub 2 data looks like this:
+    # menuentry 'CentOS (4.0.9-75_fbk14_4148_g37fe86f)' {
+    #   set root='(hd0,1)'
+    # or
+    #   set root='hd0,gpt2'
+    #
+    #
+    # Grub 1 data looks like this:
+    # title CentOS (4.0.9-68_fbk12_4058_gd84fdb3)
+    #   root (hd0,0)
+    #
+    # Extracted string will be in the form of 'hd0,2' regardless
+    def self.extract_root_device(full_fstab)
+      root_device_regexp = /^\s*[^#].*root[=\s]+['"]?\(?([\w,]+)\)?['"]?/
+      full_fstab[root_device_regexp, 1]
+    end
+
+    def self.extract_device_hints(full_fstab)
+      device_hints = []
+      full_fstab.each_line do |line|
+        # We don't care about any of the comments
+        next if line.lstrip.start_with?('#')
+        if line.lstrip.start_with?('device')
+          device_hints << line.chomp
+        end
+      end
+      device_hints
+    end
+  end
+end
