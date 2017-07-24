5206a5d9fbf52bbda6ed88c542c833d3c82de946
Phil Dibowitz <phil@ipom.com>
2017-07-24 12:58:28 -0700

    Add documentation around the sysctl idempotency code
    Summary:
    chef_test_hosts: dev3449.prn1.facebook.com
    
    The code didn't include comments on why this is necessary, so adding that.
    
    Thanks clm for catching this.
    
    Differential Revision: D5481860
    
    fbshipit-source-id: 1ac8a5d8f13372eb0e94f3d01f089b0b3435e02f

--- cookbooks/fb_sysctl/resources/default.rb
+++ cookbooks/fb_sysctl/resources/default.rb
@@ -5,6 +5,14 @@ def set_sysctl(name, val)
   s.error!
 end
 
+# Normally you'd expect to just run `sysctl -p` when you update the file.
+# However, this isn't actually idempotent. It sets every single sysctl in
+# /etc/sysctl.conf - even the ones that aren't changing... and some sysctls
+# cause actions in the kernel upon writing (like dropping caches). We want
+# to be fully idempotent and only set the things that have changed and honor
+# the expectations the Chef users have around idempotency. So here we walk
+# all of the ones we expect to be set and update only those that are not
+# correct.
 action :apply do
   bad_settings = FB::Sysctl.incorrect_settings(
     FB::Sysctl.current_settings,
