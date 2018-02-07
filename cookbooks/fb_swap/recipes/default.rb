1f684bd7840a1a685d9c6f10b45fe36c1310a2cd
Yang Xia <yangxia@fb.com>
2018-02-07 08:29:54 -0800

    Turn up encrypted swap recipe from 20% to 50%
    Summary:
    
    Continue enabling encrypted swap
    
    Reviewed By: patcox
    
    Differential Revision: D6913466
    
    fbshipit-source-id: 21b57d175bb9cd18858a4e08f93f4627b818ae8f

--- cookbooks/fb_swap/recipes/default.rb
+++ cookbooks/fb_swap/recipes/default.rb
@@ -12,7 +12,7 @@
 # of patent rights can be found in the PATENTS file in the same directory.
 #
 
-if node.in_shard?(19)
+if node.in_shard?(49)
   include_recipe 'fb_swap::new'
 else
   include_recipe 'fb_swap::old'
