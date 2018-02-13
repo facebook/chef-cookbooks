94a4952611ef7eae783d29202faa670a6771e8bd
Naomi Reeves <naomi.c.reeves@gmail.com>
2018-02-12 17:07:07 -0800

    update modprobe resource to be compliant
    Summary:
    Remove deprecations, make compliant for Chef 13
    
    Eliminated following deprecations to prepare for Chef13:
    [Resource Property Name Collision](https://docs.chef.io/deprecations_property_name_collision.html)
    We had defined properties (`retries` and `retry_delay`) on our custom resource
    that already existed.
    [Custom Resource Cleanups](https://docs.chef.io/deprecations_custom_resource_cleanups.html)
    * We were using the old `attributes` reference, has been renamed to be `property`
    in attempt to eliminate confusion
    * moved the provider logic in to the resource, removed `converge_by` and
    `updated_by_last_action` since we are using Chef native resources that can
    control resource state and send any notifications correctly.
    
    Reviewed By: jaymzh
    
    Differential Revision: D6971040
    
    fbshipit-source-id: 835152275d3621632e5eb32295edc40bbd25bd36

--- cookbooks/fb_modprobe/resources/module.rb
+++ cookbooks/fb_modprobe/resources/module.rb
@@ -6,13 +6,80 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 #
-actions :load, :unload
 default_action :load
 
-attribute :module_name, :kind_of => String, :name_attribute => true
-attribute :verbose, :kind_of => [TrueClass, FalseClass], :default => false
-attribute :timeout, :kind_of => Integer, :default => 300
-attribute :fallback, :kind_of => [TrueClass, FalseClass], :default => false
-attribute :module_params, :kind_of => [String, Array], :required => false
-attribute :retries, :kind_of => Integer, :default => 0
-attribute :retry_delay, :kind_of => Integer, :default => 2
+property :module_name, :kind_of => String, :name_property => true
+property :verbose, :kind_of => [TrueClass, FalseClass], :default => false
+property :timeout, :kind_of => Integer, :default => 300
+property :fallback, :kind_of => [TrueClass, FalseClass], :default => false
+property :module_params, :kind_of => [String, Array], :required => false
+
+def whyrun_supported?
+  true
+end
+
+action_class do
+  def modprobe_module(new_resource, unload)
+    module_name = new_resource.module_name
+    params = [new_resource.module_params].flatten.compact
+    timeout = new_resource.timeout
+    verbose = new_resource.verbose
+    fallback = new_resource.fallback
+
+    flags = []
+    flags << '-v' if verbose
+    flags << '-r' if unload
+
+    # Correctly handle built-in modules. If no parameters were supplied, we
+    # just return true. If the caller supplied parameters, we fail the Chef run
+    # and ask them to fix their cookbook, since we can't apply them.
+    if ::File.exist?("/sys/module/#{module_name}")
+      unless ::File.exist?("/sys/module/#{module_name}/initstate")
+        ::Chef::Log.warn(
+          "fb_modprobe called on built-in module '#{module_name}'",
+        )
+        unless params.empty?
+          fail <<-FAIL
+          Cannot set parameters for built-in module '#{module_name}'!
+          Parameters for built-in modules must be passed on the kernel cmdline.
+          Prefix the parameter with the module name and a dot.
+          Examples: "ipv6.autoconf=1", "mlx4_en.udp_rss=1"
+          FAIL
+        end
+        return True
+      end
+    end
+
+    command = ['/sbin/modprobe'] + flags + [module_name] + params
+
+    # Sometimes modprobe fails, like when the module was uninstalled
+    if fallback && unload
+      command << '||'
+      command << 'rmmod'
+      command << '-v' if verbose
+      command << module_name
+    end
+
+    execute command.join(' ') do
+      action :run
+      notifies :reload, 'ohai[reload kernel]', :immediately
+      timeout timeout
+    end
+  end
+end
+
+action :load do
+  if FB::Modprobe.module_loaded?(new_resource.module_name)
+    Chef::Log.debug("#{new_resource}: Module already loaded")
+  else
+    modprobe_module(new_resource, false)
+  end
+end
+
+action :unload do
+  if !FB::Modprobe.module_loaded?(new_resource.module_name)
+    Chef::Log.debug("#{new_resource}: Module already unloaded")
+  else
+    modprobe_module(new_resource, true)
+  end
+end
