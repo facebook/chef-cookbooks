0d4cec6b79fc4f7ad6052ffd04d96d0776692c8d
Naomi Reeves <naomi.c.reeves@gmail.com>
2018-06-14 11:16:35 -0700

    rewrite rsync provider
    Summary: Move fb_rsync to a custom resource and make it chef 13 compliant. I'm not refactoring the code, just migrating it. I will refactor in later diffs.
    
    Differential Revision: D8408905
    
    fbshipit-source-id: dfcd53026607257ad6f75a575d0479bdb6a5d5f6

--- cookbooks/fb_rsync/resources/default.rb
+++ cookbooks/fb_rsync/resources/default.rb
@@ -7,15 +7,90 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-actions :sync
-default_action :sync
-attribute :destination, :kind_of => String, :name_attribute => true,
-                        :required => true
-attribute :source, :kind_of => String, :required => true
-attribute :sharddelete, :kind_of => [TrueClass, FalseClass], :default => false
-attribute :sharddeleteexcluded, :kind_of => [TrueClass, FalseClass],
-                                :default => false
-attribute :extraopts, :kind_of => String, :default => ''
-attribute :partial, :kind_of => [TrueClass, FalseClass], :default => true
-attribute :timeout, :kind_of => Integer, :default => 60
-attribute :maxdelete, :kind_of => Integer, :default => 100
+property :destination, String, :name_property => true, :required => true
+property :source, String, :required => true
+property :sharddelete, [TrueClass, FalseClass], :default => false
+property :sharddeleteexcluded, [TrueClass, FalseClass], :default => false
+property :extraopts, String, :default => ''
+property :partial, [TrueClass, FalseClass], :default => true
+property :timeout, Integer, :default => 60
+property :maxdelete, Integer, :default => 100
+
+action_class do
+  include FB::Rsync::Helpers
+
+  def runcmd(cmd, dest)
+    Chef::Log.debug("fb_rsync[#{dest}]: Running command: #{cmd}")
+    # Throw normal errors except on 25 which is a max-delete limit error
+    s = Mixlib::ShellOut.new(cmd, :returns => [0, 25])
+    s.run_command.error!
+    Chef::Log.debug("fb_rsync[#{dest}]: STDOUT:\n#{s.stdout}")
+    Chef::Log.debug("fb_rsync[#{dest}]: STDERR:\n#{s.stderr}")
+    return s.exitstatus
+  end
+end
+
+action :sync do
+  # convenience vars
+  src = determine_src(new_resource.source)
+  dest = new_resource.destination
+  exopts = new_resource.extraopts
+  maxdel = new_resource.maxdelete
+  delexclude = new_resource.sharddeleteexcluded
+  dodelete = false
+
+  # Set some basic options to build on
+  opts = '-avz --partial --partial-dir=.rsync-partial ' +
+      "--timeout=#{new_resource.timeout}"
+
+  if new_resource.sharddelete
+    deletehour = node.get_flexible_shard(24)
+
+    if deletehour == Time.at(node['ohai_time']).hour
+      Chef::Log.info("fb_rsync[#{dest}]: Sharded deletes will happen " +
+          'on this run')
+      if delexclude
+        # delete-excluded implies delete so we only need one or the other
+        opts += ' --delete-excluded'
+      else
+        opts += ' --delete'
+      end
+      # maxdel can be nil if someone really wants to turn it off
+      opts += " --max-delete=#{maxdel}" if maxdel
+      dodelete = true
+    else
+      Chef::Log.info("fb_rsync[#{dest}]: Skipping deletes because our " +
+          "shard only deletes during hour #{deletehour}")
+    end
+  end
+
+  # Add the user's extra opts. God help them.
+  opts += " #{exopts}" unless exopts.empty?
+
+  # If we plan to delete with maxdelete, we need to dryrun because we won't
+  # delete anything if it'll result in a partial delete due to maxdelete setting
+  if dodelete && maxdel
+    cmd = "rsync #{opts} --dry-run #{src} #{dest}"
+    status = runcmd(cmd, dest)
+    if status == 25
+      # Echo the command in the log at info level so folks can easily find what
+      # needs deleting while troubleshooting outside the sharddelete hour
+      Chef::Log.info(
+        "fb_rsync[#{dest}]: Refusing to continue after running command: #{cmd}",
+      )
+      fail FB::Rsync::MaxDeleteLimit, "fb_rsync[#{dest}]: Rsync will result " +
+        "in deleting more than --max-delete limit (#{maxdel}), cowardly " +
+        'refusing to continue.'
+    end
+  end
+
+  # Actually run the rsync
+  execute "rsync #{dest}" do
+    command "rsync #{opts} #{src} #{dest}"
+    action :run
+  end
+
+  # We always run the rsync and there's no way to know if it changed anything
+  # so if we got this far we have to assume we did
+  Chef::Log.info("fb_rsync[#{dest}]: Rsync complete")
+end
