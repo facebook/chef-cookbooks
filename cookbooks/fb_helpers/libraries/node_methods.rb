941b54844d1279bec244f6d84684cbc5a541dd35
Davide Cavalca <dcavalca@fb.com>
2018-08-08 10:58:24 -0700

    fb_helpers: add cloud node methods
    Summary:
    
    These just wrap the ohai cloud plugin
    
    Reviewed By: NaomiReeves
    
    Differential Revision: D9221914
    
    fbshipit-source-id: 31ca243299408f4a1729d6af89ae95807523d0d8

--- cookbooks/fb_helpers/libraries/node_methods.rb
+++ cookbooks/fb_helpers/libraries/node_methods.rb
@@ -102,6 +102,14 @@ class Chef
       return File.directory?('/vagrant')
     end
 
+    def cloud?
+      return self['cloud'] && !self['cloud']['provider'].nil?
+    end
+
+    def aws?
+      return self.cloud? && self['cloud']['provider'] == 'ec2'
+    end
+
     # Take a string representing a mount point, and return the
     # device it resides on.
     def device_of_mount(m)
