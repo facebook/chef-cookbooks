31a4d69a5b1afe1bb385341495bc87749317d9cf
chrisrlong <chrisrlong@users.noreply.github.com>
2019-03-12 13:52:47 -0700

    Updating keys.rb to use long format fingerprint (#53)
    Summary:
    Updating the regex in keys.rb to use match on long format fingerprint to get the key ID as the short form is removed in newer versions of apt-key.
    Pull Request resolved: https://github.com/facebook/chef-cookbooks/pull/53
    
    Reviewed By: petermblair
    
    Differential Revision: D14425825
    
    Pulled By: petermblair
    
    fbshipit-source-id: 9f1893dd4b9d35f844cd29a6f74b8d6b59a4126c

--- cookbooks/fb_apt/providers/keys.rb
+++ cookbooks/fb_apt/providers/keys.rb
@@ -22,13 +22,13 @@ action :run do
   if keys && keyring
     installed_keys = []
     if ::File.exist?(keyring)
-      cmd = Mixlib::ShellOut.new("LANG=C apt-key --keyring #{keyring} list")
+      cmd = Mixlib::ShellOut.new("LANG=C apt-key --keyring #{keyring} finger")
       cmd.run_command
       cmd.error!
       output = cmd.stdout.split("\n")
       Chef::Log.debug("apt-key output: #{output.join("\n")}")
-      installed_keys = output.select { |x| x.start_with?('pub') }.map do |x|
-        x[%r{pub.*/(?<keyid>[A-Z0-9]*)}, 'keyid']
+      installed_keys = output.select { |x| x.match(/(\s\w{4}){5}/) }.map do |x|
+        x[/(?<keyid>[\w]{4}\s[\w]{4})$/, 'keyid'].delete(' ')
       end
     end
     Chef::Log.debug("Installed keys: #{installed_keys.join(', ')}")
