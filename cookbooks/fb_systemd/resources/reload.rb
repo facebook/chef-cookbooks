81bc11d190fd520c6ee688311536c4c2ce0fdeaa
Anita Zhang <anitazha@fb.com>
2019-07-17 10:43:14 -0700

    support "reexec" option in reload custom resource
    Summary:
    Refactors the fb_systemd_reload stuff so it can be reused to implement reexec using the same code. The reexec is used during systemd upgrades.
    
    This should be done as part of the RPM installation but there is no built in way to `systemctl daemon-reexec` for all running user managers (upstream issue filed https://github.com/systemd/systemd/issues/13065).
    
    Reviewed By: NaomiReeves
    
    Differential Revision: D16274280
    
    fbshipit-source-id: c557a9bf2c5d9502d8e926d6616f24bfca960dd9

--- cookbooks/fb_systemd/resources/reload.rb
+++ cookbooks/fb_systemd/resources/reload.rb
@@ -15,8 +15,92 @@
 # limitations under the License.
 #
 
-actions :run
-default_action :run
+default_action :reload
 
-attribute :instance, :kind_of => String, :default => 'system'
-attribute :user, :kind_of => [String, NilClass], :default => nil
+property :instance, :kind_of => String, :default => 'system'
+property :user, :kind_of => [String, NilClass], :default => nil
+
+def whyrun_supported?
+  true
+end
+
+action_class do
+  def daemon_reload_or_reexec(action)
+    unless node.systemd?
+      # The predicate used in this node method looks for /run/systemd/system
+      # which is only present on a booted system. This resource is only useful
+      # if the system is booted as it interacts with systemd. If the
+      # system is not booted, then we can safely skip.
+      Chef::Log.info(
+        "Requested to #{action} systemd #{new_resource.instance} " +
+        'skipped because the system is not booted',
+      )
+      return
+    end
+    case new_resource.instance
+    when 'system'
+      execute "#{action} systemd system instance" do
+        command "/bin/systemctl daemon-#{action}"
+      end
+    when 'user'
+      unless node['sessions'] && node['sessions']['by_user']
+        Chef::Log.info("Requested to #{action} systemd user instance for all " +
+                       "users, but there are no sessions to #{action}")
+        return
+      end
+      logged_in = node['sessions']['by_user'].keys
+      if new_resource.user
+        unless logged_in.include?(new_resource.user)
+          Chef::Log.info("Requested to #{action} systemd user instance for " +
+                         "#{new_resource.user} but it's not a logged in user")
+          return
+        end
+        users = [new_resource.user]
+      else
+        users = node['sessions']['by_user'].to_hash.keys.sort
+      end
+      failed = false
+      users.each do |user|
+        begin
+          uid = Etc.getpwnam(user).uid
+        rescue ArgumentError
+          Chef::Log.error(
+            "fb_systemd_reload: user '#{user}' is not defined, " +
+            'will abort later.',
+          )
+          failed = true
+          next
+        end
+        bus_path = "/run/user/#{uid}/bus"
+        execute "#{action} systemd --user for #{user}" do
+          only_if { ::File.exist?(bus_path) }
+          command "/bin/systemctl --user daemon-#{action}"
+          environment 'DBUS_SESSION_BUS_ADDRESS' => "unix:path=#{bus_path}"
+          user user
+          # This can fail if the session is in a weird state, which is fine, as
+          # it'll get respawned of the next login (which has the same effect
+          # as running the action).
+          ignore_failure true
+        end
+      end
+      if failed
+        fail 'fb_systemd_reload: aborting due to previous failures'
+      end
+    else
+      fail "fb_systemd_reload: instance type '#{new_resource.instance}' " +
+        'is not supported!'
+    end
+  end
+end
+
+action :run do
+  daemon_reload_or_reexec('reload')
+end
+
+action :reload do
+  daemon_reload_or_reexec('reload')
+end
+
+action :reexec do
+  daemon_reload_or_reexec('reexec')
+end
