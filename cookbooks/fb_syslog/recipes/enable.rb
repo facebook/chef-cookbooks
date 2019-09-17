e7cbd3f4822c298a8043ea4e7d36b9bdf4f05d40
Davide Cavalca <dcavalca@fb.com>
2019-09-17 09:52:42 -0700

    fb_syslog: gate some systemd-specific stuff
    Summary: This should unbreak older ubuntu and centos
    
    Reviewed By: nikitakoshikov
    
    Differential Revision: D17414595
    
    fbshipit-source-id: 2a86b2c2b383b99d0d2cf2a60b4bc5f6fc9d35a9

--- cookbooks/fb_syslog/recipes/enable.rb
+++ cookbooks/fb_syslog/recipes/enable.rb
@@ -1,6 +1,7 @@
 # this is almost identical to running 'systemctl enable rsyslog', except that it
 # has no run-time requirements and can be run while setting up a container.
 link '/etc/systemd/system/syslog.service' do
+  only_if { node.systemd? }
   to '/usr/lib/systemd/system/rsyslog.service'
   owner 'root'
   group 'root'
