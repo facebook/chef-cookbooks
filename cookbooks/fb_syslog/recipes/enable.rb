9a25021d733cc74963ccdcd9d375ba8f190f776d
Davide Cavalca <dcavalca@fb.com>
2019-09-19 07:36:27 -0700

    fb_syslog: add missing copyright headers
    Summary: Make the compliance tool happy
    
    Reviewed By: joshuamiller01
    
    Differential Revision: D17472123
    
    fbshipit-source-id: b770c4931bc3f7fb1f295cdd63c577800a16a4fa

--- cookbooks/fb_syslog/recipes/enable.rb
+++ cookbooks/fb_syslog/recipes/enable.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: fb_syslog
+# Recipe:: enable
+#
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # this is almost identical to running 'systemctl enable rsyslog', except that it
 # has no run-time requirements and can be run while setting up a container.
 link '/etc/systemd/system/syslog.service' do
