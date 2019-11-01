9b7d9602dcad4ff5e8e43065287ec6658a81cfb0
Phil Dibowitz <phil@ipom.com>
2019-11-01 12:25:41 -0700

    lint

--- cookbooks/fb_sssd/metadata.rb
+++ cookbooks/fb_sssd/metadata.rb
@@ -18,9 +18,12 @@
 name 'fb_sssd'
 maintainer 'Facebook'
 maintainer_email 'noreply@facebook.com'
-license 'All Rights Reserved'
+license 'Apache 2.0'
 source_url 'https://github.com/facebook/chef-cookbooks/'
 description 'Installs/Configures sssd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+supports 'centos'
+supports 'debian'
+supports 'ubuntu'
 # never EVER change this number, ever.
 version '0.1.0'
