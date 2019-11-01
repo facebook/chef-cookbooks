d212c4fee210d1a5f5ad637c5b3e9aa19d419847
Phil Dibowitz <phil@ipom.com>
2019-11-01 12:32:21 -0700

    lint

--- cookbooks/fb_stunnel/metadata.rb
+++ cookbooks/fb_stunnel/metadata.rb
@@ -18,9 +18,13 @@
 name 'fb_stunnel'
 maintainer 'Facebook'
 maintainer_email 'noreply@facebook.com'
-license 'All Rights Reserved'
+license 'Apache 2.0'
 source_url 'https://github.com/facebook/chef-cookbooks/'
 description 'Installs/Configures fb_stunnel'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 # never EVER change this number, ever.
 version '0.1.0'
+supports 'centos'
+supports 'debian'
+supports 'ubuntu'
+supports 'freebsd'
