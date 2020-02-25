b01d22719f5248b8f40fd9763fe7bfa281f83626
Davide Cavalca <dcavalca@fb.com>
2020-02-25 11:35:42 -0800

    chef: fix foodcritic in github (#111)
    Summary:
    Pull Request resolved: https://github.com/facebook/chef-cookbooks/pull/111
    
    Add some debugging to make this easier to sort out
    
    Differential Revision: D20099092
    
    fbshipit-source-id: bc8c65e90f3e30c3ffce4d623bd17c04d39e69d2

--- cookbooks/fb_nscd/metadata.rb
+++ cookbooks/fb_nscd/metadata.rb
@@ -7,3 +7,8 @@ description 'Installs/Configures nscd'
 source_url 'https://github.com/facebook/chef-cookbooks/'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.1'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'redhat'
+supports 'ubuntu'
