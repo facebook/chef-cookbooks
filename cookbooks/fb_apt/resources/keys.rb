c2d13028c377054f02e3735d6904cda5be5cdd54
Naomi Reeves <naomi.c.reeves@gmail.com>
2020-05-07 11:43:01 -0700

    providers => custom resources: fb_apt
    Summary:
    Converting `fb_apt` custom resources over to modern chef syntax for the next chef upgrade
    
    It appears that we aren't actually running this cookbook on the fleet, with the exception of the `fb_init_sample` on github.
    
    Reviewed By: davide125
    
    Differential Revision: D21445797
    
    fbshipit-source-id: 66f04f261129db3665534e85978eaa94ab40fc54

--- cookbooks/fb_apt/resources/keys.rb
+++ cookbooks/fb_apt/resources/keys.rb
@@ -16,5 +16,69 @@
 # limitations under the License.
 #
 
-actions :run
-default_action :run
+def whyrun_supported?
+  true
+end
+
+action :run do
+  keyserver = node['fb_apt']['keyserver']
+  desired_keys = node['fb_apt']['keys'].to_hash
+
+  if desired_keys
+    installed_keys = FB::Apt.get_installed_keyids(node)
+    Chef::Log.debug(
+      "fb_apt[keys]: Installed keys: #{installed_keys.join(', ')}",
+    )
+
+    legit_keyrings = FB::Apt._get_owned_keyring_files(node)
+    Dir.glob('/etc/apt/trusted.gpg.d/*').each do |keyring|
+      next if legit_keyrings.include?(keyring)
+
+      if node['fb_apt']['preserve_unknown_keyrings']
+        Chef::Log.warn(
+          "fb_apt[keys]: Unknown keyring #{keyring} being preserved!",
+        )
+      else
+        file keyring do
+          action :delete
+        end
+      end
+    end
+
+    # Process keys to add
+    desired_keys.each do |keyid, key|
+      if installed_keys.include?(keyid)
+        Chef::Log.debug(
+          "fb_apt[keys]: Skipping keyid #{keyid} as it's already registered",
+        )
+      else
+        Chef::Log.debug("fb_apt[keys]: Processing new keyid #{keyid}")
+        if key
+          execute "add key for #{keyid} to APT" do
+            command "echo '#{key}' | apt-key add -"
+          end
+        elsif keyserver
+          execute "fetch and add key for keyid #{keyid} to APT" do
+            command "apt-key adv --keyserver #{keyserver} --recv #{keyid}"
+            # with the DDOS against PGP Keyservers, we need to try
+            # several times
+            retries 2
+          end
+        else
+          fail "Cannot fetch key for #{keyid} as keyserver is not defined"
+        end
+      end
+    end
+
+    # Process keys to remove
+    installed_keys.each do |keyid|
+      if desired_keys.keys.include?(keyid)
+        Chef::Log.debug("fb_apt[keys]: Not deleting added keyid #{keyid}")
+      else
+        execute "delete key for #{keyid} from APT" do
+          command "apt-key del #{keyid}"
+        end
+      end
+    end
+  end
+end
