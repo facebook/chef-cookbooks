e01eeb3405021b715009919209955c311d3b2007
Imanol-Mikel Barba <ibarba@fb.com>
2020-11-11 03:45:12 -0800

    fb_grub: restrict grub.cfg permissions 50% -> 100%
    Summary: Bumping D24481065 (https://github.com/facebook/chef-cookbooks/commit/c97b122ffc1a924f6c0d4bf80cc40c6fc9cad4ad) to 50%
    
    Reviewed By: malmond77
    
    Differential Revision: D24852021
    
    fbshipit-source-id: 72d1e35d5b884922d3106131b77b1d2e313b493b

--- cookbooks/fb_grub/recipes/config.rb
+++ cookbooks/fb_grub/recipes/config.rb
@@ -242,17 +242,13 @@ end
     # No "mode" for EFI since mode is determined by mount options,
     # not files
     if type == 'bios'
-      if node.in_shard?(50)
-        mode lazy {
-          if node['fb_grub']['users'].empty?
-            '0644'
-          else
-            '0600'
-          end
-        }
-      else
-        mode '0644'
-      end
+      mode lazy {
+        if node['fb_grub']['users'].empty?
+          '0644'
+        else
+          '0600'
+        end
+      }
     end
     variables(
       {
