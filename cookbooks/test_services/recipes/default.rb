5825c7027bec594fac08664ef1aebdb3a2e77c76
Davide Cavalca <dcavalca@fb.com>
2021-04-01 11:13:44 -0700

    fb_zfs: delete deprecated cookbook (#168)
    Summary:
    Pull Request resolved: https://github.com/facebook/chef-cookbooks/pull/168
    
    `fb_zfs` has been effectively unmaintained for several years, and has long standing issues like https://github.com/facebook/chef-cookbooks/issues/79 that aren't trivial to fix without a real deployment to test it on.
    
    Reviewed By: nikitakoshikov
    
    Differential Revision: D23934240
    
    fbshipit-source-id: 51d839e780be7583c49da0e8c78ed8dc24c0a4a4

--- cookbooks/test_services/recipes/default.rb
+++ cookbooks/test_services/recipes/default.rb
@@ -32,7 +32,3 @@ end
 # Currently fb_reprepro is broken
 # https://github.com/facebook/chef-cookbooks/issues/78
 # include_recipe 'fb_reprepro'
-
-# Currently fb_zfs is broken
-# https://github.com/facebook/chef-cookbooks/issues/79
-# include_recipe 'fb_zfs'
