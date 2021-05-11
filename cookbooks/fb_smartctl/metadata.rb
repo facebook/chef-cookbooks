b24b68a50bfc5e2100400a7dfe792022b2a40f5a
Davide Cavalca <dcavalca@fb.com>
2021-05-11 15:56:16 -0700

    fb_smartctl: add copyright byline
    Summary: Make the bot happy
    
    Reviewed By: NaomiReeves
    
    Differential Revision: D28366269
    
    fbshipit-source-id: 49e38d0239cb8ba2a4780f1ef94f0cd9a94c8a9a

--- cookbooks/fb_smartctl/metadata.rb
+++ cookbooks/fb_smartctl/metadata.rb
@@ -1,4 +1,4 @@
-# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+# Copyright (c) 2021-present, Facebook, Inc.
 name 'fb_smartctl'
 maintainer 'Facebook'
 maintainer_email 'noreply@facebook.com'
