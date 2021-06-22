cea3d3fcf7ff1ec3b5082863c772f7ed20741904
Joshua Miller <joshuamiller01@gmail.com>
2021-06-22 16:12:28 -0700

    Change cookbook ownership from oscore to new owners
    Summary:
    Change the cookbook metadata to have the new owners for various cookbooks
    
    Reviewed By: ifel
    
    Differential Revision: D29215868
    
    fbshipit-source-id: 22462af9be9815a24771e0c0c535dbc74a5a4a5d

--- cookbooks/fb_dcrpm/metadata.rb
+++ cookbooks/fb_dcrpm/metadata.rb
@@ -1,8 +1,8 @@
 # Copyright (c) 2016-present, Facebook, Inc.
 name 'fb_dcrpm'
 maintainer 'Facebook'
-maintainer_email 'oncall+oscore@xmail.facebook.com'
-license 'All rights reserved'
+maintainer_email 'noreply@facebook.com'
+license 'Apache-2.0'
 description 'Installs/Configures dcrpm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
