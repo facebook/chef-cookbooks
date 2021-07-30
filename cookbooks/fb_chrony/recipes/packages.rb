57c4ffd29d740b2960dadb33de157354549a81e2
Edward Pilatowicz <epilatow@fb.com>
2021-07-30 10:57:51 -0700

    chrony-4.1-1.el8 is breaking things, pin chrony-3.5-2.el8
    Summary:
    
    Reviewed By: leoleovich
    
    Differential Revision: D30017413
    
    fbshipit-source-id: 77b47adea6f20e9a97551566b028e0347d071e60

--- cookbooks/fb_chrony/recipes/packages.rb
+++ cookbooks/fb_chrony/recipes/packages.rb
@@ -19,7 +19,15 @@
 # limitations under the License.
 #
 
-package 'chrony' do
-  only_if { node['fb_chrony']['manage_packages'] }
-  action :upgrade
+if node.centos8?
+  # We don't have any rolloug phases yet.
+  node.default['fb_slowroll']['fb_chrony']['phases'] = []
+  fb_slowroll 'chrony' do
+    only_if { node['fb_chrony']['manage_packages'] }
+  end
+else
+  package 'chrony' do
+    only_if { node['fb_chrony']['manage_packages'] }
+    action :upgrade
+  end
 end
