b9bcb7ba775f0aa2a9768431047414e076bd9256
Anita Zhang <anitazha@fb.com>
2021-11-18 13:24:35 -0800

    fb_networkd: handle leftover fb_networkd configs
    Summary:
    If we have configs/interfaces that were previously created through `fb_networkd` and either are no longer referenced or were changed (by changing the priority) then we should show that as a pending network change and clean them up when network changes are allowed.
    
    For interfaces that are still managed through fb_networkd but have old configs on the host, we delete those files and `networkctl reload` to apply changes (when network changes are allowed).
    
    For interfaces that are no longer managed, we delete those files and take down the interface and `networkctl reload` to get the final state (when network changes are allowed).
    
    Reviewed By: joshuamiller01
    
    Differential Revision: D32181988
    
    fbshipit-source-id: 3e24d3ee8558c07bbdaf313c9ca70487a323b148

--- cookbooks/fb_networkd/resources/default.rb
+++ cookbooks/fb_networkd/resources/default.rb
@@ -30,11 +30,22 @@ action :manage do
          'names conflict: ' + dup_names.join(', ').to_s
   end
 
+  managed_networks = []
+  managed_links = []
+  managed_netdevs = []
   config_glob = ::File.join(
     FB::Networkd::BASE_CONFIG_PATH, '*.{network,netdev,link}'
   )
   Dir.glob(config_glob).each do |path|
-    unless ::File.basename(path).include?('-fb_networkd-')
+    if ::File.basename(path).include?('-fb_networkd-')
+      if path.end_with?('.network')
+        managed_networks << path
+      elsif path.end_with?('.link')
+        managed_links << path
+      elsif path.end_with?('.netdev')
+        managed_netdevs << path
+      end
+    else
       Chef::Log.warn("fb_networkd: Unmanaged network config #{path} found")
     end
   end
@@ -65,6 +76,41 @@ action :manage do
       "#{conf['priority']}-fb_networkd-#{conf['name']}.network",
     )
 
+    # This file is actively managed and already exists on the host so remove it
+    # from the "manageds" array.
+    remove_conflicts = false
+    if managed_networks.include?(conffile)
+      managed_networks.delete(conffile)
+      remove_conflicts = true
+    end
+
+    # If this config was previously managed under a different name (e.g.
+    # different priority) then set up a file resource to allow it to be deleted
+    # when the new config is set up or if the "new" config already exists.
+    conflicting_networks = managed_networks.grep(
+      /-fb_networkd-#{conf['name']}.network$/,
+    )
+    conflicting_networks ||= []
+    conflicting_networks.each do |path|
+      managed_networks.delete(path)
+
+      file path do
+        only_if do
+          node.interface_change_allowed?(conf['name']) && remove_conflicts
+        end
+        owner node.root_user
+        group node.root_group
+        mode '0644'
+        action :delete
+        notifies :run, 'execute[networkctl reload]'
+        notifies :run, "execute[networkctl reconfigure #{conf['name']}]"
+      end
+
+      if !node.interface_change_allowed?(conf['name']) && remove_conflicts
+        FB::Helpers._request_nw_changes_permission(run_context, new_resource)
+      end
+    end
+
     fb_helpers_gated_template conffile do # ~FB031
       allow_changes node.interface_change_allowed?(conf['name'])
       source 'networkd.conf.erb'
@@ -74,8 +120,8 @@ action :manage do
       variables(
         :config => conf['config'],
       )
-      notifies :run, 'execute[networkctl reload]', :immediately
-      notifies :run, "execute[networkctl reconfigure #{conf['name']}]", :delayed
+      notifies :run, 'execute[networkctl reload]'
+      notifies :run, "execute[networkctl reconfigure #{conf['name']}]"
     end
   end
 
@@ -104,6 +150,40 @@ action :manage do
       "#{conf['priority']}-fb_networkd-#{conf['name']}.link",
     )
 
+    # This file is actively managed and already exists on the host so remove it
+    # from the "manageds" array.
+    remove_conflicts = false
+    if managed_links.include?(conffile)
+      managed_links.delete(conffile)
+      remove_conflicts = true
+    end
+
+    # If this config was previously managed under a different name (e.g.
+    # different priority) then set up a file resource to allow it to be deleted
+    # when the new config is set up or if the "new" config already exists.
+    conflicting_links = managed_links.grep(
+      /-fb_networkd-#{conf['name']}.link$/,
+    )
+    conflicting_links ||= []
+    conflicting_links.each do |path|
+      managed_links.delete(path)
+
+      file path do
+        only_if do
+          node.interface_change_allowed?(conf['name']) && remove_conflicts
+        end
+        owner node.root_user
+        group node.root_group
+        mode '0644'
+        action :delete
+        notifies :run, "execute[udevadm trigger #{conf['name']}]"
+      end
+
+      if !node.interface_change_allowed?(conf['name']) && remove_conflicts
+        FB::Helpers._request_nw_changes_permission(run_context, new_resource)
+      end
+    end
+
     fb_helpers_gated_template conffile do # ~FB031
       allow_changes node.interface_change_allowed?(conf['name'])
       source 'networkd.conf.erb'
@@ -113,7 +193,7 @@ action :manage do
       variables(
         :config => conf['config'],
       )
-      notifies :run, "execute[udevadm trigger #{conf['name']}]", :delayed
+      notifies :run, "execute[udevadm trigger #{conf['name']}]"
     end
   end
 
@@ -146,6 +226,41 @@ action :manage do
       "#{conf['priority']}-fb_networkd-#{conf['name']}.netdev",
     )
 
+    # This file is actively managed and already exists on the host so remove it
+    # from the "manageds" array.
+    remove_conflicts = false
+    if managed_netdevs.include?(conffile)
+      managed_netdevs.delete(conffile)
+      remove_conflicts = true
+    end
+
+    # If this config was previously managed under a different name (e.g.
+    # different priority) then set up a file resource to allow it to be deleted
+    # when the new config is set up or if the "new" config already exists.
+    conflicting_netdevs = managed_netdevs.grep(
+      /-fb_networkd-#{conf['name']}.netdev$/,
+    )
+    conflicting_netdevs ||= []
+    conflicting_netdevs.each do |path|
+      managed_netdevs.delete(path)
+
+      file path do
+        only_if do
+          node.interface_change_allowed?(conf['name']) && remove_conflicts
+        end
+        owner node.root_user
+        group node.root_group
+        mode '0644'
+        action :delete
+        notifies :run, 'execute[networkctl reload]'
+        notifies :run, "execute[networkctl reconfigure #{conf['name']}]"
+      end
+
+      if !node.interface_change_allowed?(conf['name']) && remove_conflicts
+        FB::Helpers._request_nw_changes_permission(run_context, new_resource)
+      end
+    end
+
     fb_helpers_gated_template conffile do # ~FB031
       allow_changes node.interface_change_allowed?(conf['name'])
       source 'networkd.conf.erb'
@@ -155,8 +270,82 @@ action :manage do
       variables(
         :config => conf['config'],
       )
-      notifies :run, 'execute[networkctl reload]', :immediately
-      notifies :run, "execute[networkctl reconfigure #{conf['name']}]", :delayed
+      notifies :run, 'execute[networkctl reload]'
+      notifies :run, "execute[networkctl reconfigure #{conf['name']}]"
+    end
+  end
+
+  # For each managed file, check if we can make network changes on the
+  # inteface. If we can, then take down the interface (except links) and delete
+  # the file.
+  managed_networks.each do |path|
+    iface = path[/-fb_networkd-(.*?).network/m, 1]
+
+    if iface
+      execute "networkctl down #{iface}" do
+        only_if { node.interface_change_allowed?(iface) }
+        command "/bin/networkctl down #{iface}"
+        ignore_failure true # if the interface was not up, already down, etc.
+        action :nothing
+      end
+
+      file path do
+        only_if { node.interface_change_allowed?(iface) }
+        action :delete
+        notifies :run, "execute[networkctl down #{iface}]"
+        notifies :run, 'execute[networkctl reload]'
+      end
+
+      unless node.interface_change_allowed?(iface)
+        FB::Helpers._request_nw_changes_permission(run_context, new_resource)
+      end
+    end
+  end
+
+  managed_links.each do |path|
+    iface = path[/-fb_networkd-(.*?).link/m, 1]
+
+    if iface
+      execute "udevadm trigger #{iface}" do
+        only_if { node.interface_change_allowed?(iface) }
+        command "/bin/udevadm trigger --action=add /sys/class/net/#{iface}"
+        ignore_failure true # if the device is already down, etc.
+        action :nothing
+      end
+
+      file path do
+        only_if { node.interface_change_allowed?(iface) }
+        action :delete
+        notifies :run, "execute[udevadm trigger #{iface}]"
+      end
+
+      unless node.interface_change_allowed?(iface)
+        FB::Helpers._request_nw_changes_permission(run_context, new_resource)
+      end
+    end
+  end
+
+  managed_netdevs.each do |path|
+    iface = path[/-fb_networkd-(.*?).netdev/m, 1]
+
+    if iface
+      execute "networkctl delete #{iface}" do
+        only_if { node.interface_change_allowed?(iface) }
+        command "/bin/networkctl delete #{iface}"
+        ignore_failure true # if the interface was not up, already down, etc.
+        action :nothing
+      end
+
+      file path do
+        only_if { node.interface_change_allowed?(iface) }
+        action :delete
+        notifies :run, "execute[networkctl delete #{iface}]"
+        notifies :run, 'execute[networkctl reload]'
+      end
+
+      unless node.interface_change_allowed?(iface)
+        FB::Helpers._request_nw_changes_permission(run_context, new_resource)
+      end
     end
   end
 end
