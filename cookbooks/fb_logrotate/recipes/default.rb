45b9bcd187e0496da52bc05ba83220caf386d670
Phil Dibowitz <phil@ipom.com>
2022-02-24 12:41:02 -0800

    Fix fb_logrotate craziness (#201)
    Summary:
    
    In
    https://github.com/facebook/chef-cookbooks/commit/c5104612bf77d35474cff57e4c85ca000d848f53,
    malmond77 made the faulty claim that `whyrun_safe_ruby_block` was
    dangerous, but he misunderstood chef ordering. It's the very very last
    thing before the template (except the validator), and no one can
    possibly schedule things after it, but before the template. It's the
    whole reason why `whyrun_safe_ruby_block` exists.
    
    Further, debugging templates is difficult. And logic doesn't belong in
    templates. And certainly not munging of configs.
    
    This moves the logic back to where it belongs, being careful to also
    keep the logic fixes from that PR.
    
    Pull Request resolved: https://github.com/facebook/chef-cookbooks/pull/201
    
    Reviewed By: NaomiReeves
    
    Differential Revision: D34246543
    
    Pulled By: davide125
    
    fbshipit-source-id: a5bd1321acc16a1e7e0e38053d47e2e164ebdbfb

--- cookbooks/fb_logrotate/recipes/default.rb
+++ cookbooks/fb_logrotate/recipes/default.rb
@@ -32,6 +32,53 @@ end
 
 include_recipe 'fb_logrotate::packages'
 
+whyrun_safe_ruby_block 'munge logrotate configs' do
+  block do
+    node['fb_logrotate']['configs'].to_hash.each do |name, block|
+      time = nil
+      if block['overrides']
+        rotation = block['overrides']['rotation']
+        size = block['overrides']['size']
+
+        if rotation && size
+          fail "fb_logrotate:[#{name}]: you can only use size or rotation " +
+            'not both'
+        end
+
+        if rotation
+          # if someone wants to override weekly but didn't specify
+          # how many to keep, we default to 4
+          if rotation == 'weekly' && !block['overrides']['rotate']
+            node.default['fb_logrotate']['configs'][name][
+              'overrides']['rotate'] = '4'
+          end
+
+          if %w{hourly daily weekly monthly yearly}.include?(rotation)
+            time = rotation
+            node.rm(:fb_logrotate, :configs, name, :overrides, :rotation)
+          else
+            fail "fb_logrotate:[#{name}]: rotation #{rotation} invalid"
+          end
+        end
+
+        if size
+          time = "size #{size}"
+          node.rm(:fb_logrotate, :configs, name, :overrides, :size)
+        end
+
+        if block['overrides']['nocompress'] &&
+           node['fb_logrotate']['globals']['nocompress']
+          # redundant, remove
+          node.rm(:fb_logrotate, :configs, name, :overrides, :nocompress)
+        end
+      end
+      if time
+        node.default['fb_logrotate']['configs'][name]['time'] = time
+      end
+    end
+  end
+end
+
 whyrun_safe_ruby_block 'validate logrotate configs' do
   block do
     files = []
