b149ef6f9af0157804a4843c6c5bcdfa9e9d0041
Saravanan D <saravanand@meta.com>
2023-05-02 09:44:36 -0700

    Library functions, logic encoding raid disks, firmwares
    Summary:
    * Moved existing logic to extract disk model, firmware and disks in mdarray as `FB::Storage` methods
    * No logic change to hosts with rootfs mounted on single disk.
    * On hosts where root is mounted on mdarray, we will now encode the sorted models, relative firmwares to aggregation labels.
    ```
    # Separated by __ which is acceptable in ODS
    <CUSTOM_ROOT_DISK_MODEL>:__RAID__MZ1LB1T9HBLS-000FB__MZ1LB1T9HBLS-000FB__;
    <CUSTOM_ROOT_DISK_FIRMWARE>:__RAID__EDW76F2Q__EDW76F2Q__;
    ```
    
    Reviewed By: joshuamiller01
    
    Differential Revision: D45419604
    
    fbshipit-source-id: 25c37ad73ae55c2af609c0bf662400788baf179d

--- cookbooks/fb_storage/libraries/storage.rb
+++ cookbooks/fb_storage/libraries/storage.rb
@@ -31,6 +31,78 @@ module FB
 
     # 'size' from sysfs always assumes 512 byte blocks
     SECTOR_SIZE = 512
+
+    # Helper function to make string compatible for ODS
+    # by replacing special characters with _
+    def self.ods_compat(string)
+      string.gsub(/[^0-9a-zA-Z_-]/, '_')
+    end
+
+    # Helper function to get disk model
+    def self.get_disk_model(node, block_device_name)
+      device = node['block_device'][block_device_name]
+      if device
+        if device['model']
+          self.ods_compat(device['model'].to_s)
+        end
+      end
+    end
+
+    # Helper function to get disk firmware
+    def self.get_disk_firmware(node, block_device_name)
+      device = node['block_device'][block_device_name]
+      if device
+        # When mounted on SATA, firmware is stored on rev
+        if device['rev']
+          self.ods_compat(device['rev'].to_s)
+        # When mounted on NVME, firmware is stored on firmware_rev
+        elsif device['firmware_rev']
+          self.ods_compat(device['firmware_rev'].to_s)
+        end
+      end
+    end
+
+    # Helper function to get mdarray devices
+    def self.get_mdarray_devices(node, mdarray)
+      if node['mdadm']
+        if node['mdadm'][mdarray]
+          if node['mdadm'][mdarray]['members']
+            node['mdadm'][mdarray]['members']
+          end
+        end
+      end
+    end
+
+    # Helper function to extract sorted models and their
+    # relative firmwares of disks in mdarray with '__'
+    # delimiters
+    def self.get_mdarray_models_firmwares(node, mdarray)
+      disks_models = ''
+      disks_firmwares = ''
+      models_firmwares = []
+      devices = self.get_mdarray_devices(node, mdarray)
+      devices.each do |device|
+        disk_model = self.get_disk_model(node, device)
+        disk_firmware = self.get_disk_firmware(node, device)
+        if disk_model && disk_firmware
+          # Create a hash of model => firmware
+          model_firmware = {}
+          model_firmware[disk_model] = disk_firmware
+          # Add it to the hash array
+          models_firmwares << model_firmware
+        end
+      end
+      # Sort the hash array by model
+      models_firmwares.sort_by! { |model, _firmware| model.to_s }
+      models_firmwares.each do |model_firmware|
+        model_firmware.each do |model, firmware|
+          disks_models += model.to_s + '__'
+          disks_firmwares += firmware.to_s + '__'
+        end
+      end
+      return disks_models, disks_firmwares
+    end
+
     # Helper function for hybrid XFS users. Given the index (into
     # `eligible_devices` of the device to be used for metadata, and the number
     # of filesystems we expect to create, it will return the size of each
