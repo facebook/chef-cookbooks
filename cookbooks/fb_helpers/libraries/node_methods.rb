125d5dc6e538557747abdd6374ce23a6700f6925
John Timon <jtimon@meta.com>
2023-07-18 14:14:25 -0700

    Get chef working for rhel 9
    Summary:
    Bootstrap chef core cookbooks to RHEL 9
    
    Differential Revision: D47532272
    
    fbshipit-source-id: ff6d26338709f65e0eb17df20106761e0a2b68f2

--- cookbooks/fb_helpers/libraries/node_methods.rb
+++ cookbooks/fb_helpers/libraries/node_methods.rb
@@ -964,7 +964,7 @@ class Chef
 
     # returns the version-release of an rpm installed, or nil if not present
     def rpm_version(name)
-      if (self.centos? && !self.centos7?) || self.fedora? || self.redhat8? || self.oracle8?
+      if (self.centos? && !self.centos7?) || self.fedora? || self.redhat8? || self.oracle8? || self.redhat9?
         # returns epoch.version
         v = Chef::Provider::Package::Dnf::PythonHelper.instance.
             package_query(:whatinstalled, name).version
