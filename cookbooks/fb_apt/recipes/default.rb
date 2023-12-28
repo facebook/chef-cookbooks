add39cced83a5a51f09450ec23a698e3f9ba4dc5
Adam Saponara <as@etsy.com>
2023-12-28 13:58:47 -0500

    fb_apt: Add ability to log output of `apt-get update`

--- cookbooks/fb_apt/recipes/default.rb
+++ cookbooks/fb_apt/recipes/default.rb
@@ -18,6 +18,8 @@
 # limitations under the License.
 #
 
+require 'shellwords'
+
 unless node.debian? || node.ubuntu?
   fail 'fb_apt is only supported on Debian and Ubuntu.'
 end
@@ -94,7 +96,11 @@ fb_apt_sources_list 'populate sources list' do
 end
 
 execute 'apt-get update' do
-  command 'apt-get update'
+  command lazy do
+    log_path = node['fb_apt']['apt_update_log_path']
+    cmd_suffix = " >>#{Shellwords.shellescape(log_path)} 2>&1" if log_path
+    "apt-get update#{cmd_suffix}"
+  end
   action :nothing
 end
 
