104f824ab708dc04121e11e050e03d9308ccf1fd
Davide Cavalca <dcavalca@meta.com>
2024-02-23 07:43:23 -0800

    fb_postfix: default to lmdb maps for el10 and later (#235)
    Summary:
    
    Upstream dropped support for libdb in
    https://src.fedoraproject.org/rpms/postfix/c/6a2621e4d73d59337dc64ba45922132286b841a1
    so we need to use lmdb in RHEL 10 and later
    
    Differential Revision: D54077466

--- cookbooks/fb_postfix/recipes/default.rb
+++ cookbooks/fb_postfix/recipes/default.rb
@@ -93,6 +93,14 @@ execute 'postalias /etc/postfix/aliases' do
   action :nothing
 end
 
+# RHEL 10 and later don't support hash maps anymore
+# https://src.fedoraproject.org/rpms/postfix/c/6a2621e4d73d59337dc64ba45922132286b841a1
+if node.el_min_version?(10) || node.eln?
+  map_type = 'lmdb'
+else
+  map_type = 'hash'
+end
+
 %w{
   access
   canonical
@@ -115,14 +123,14 @@ end
     else
       mode '0644'
     end
-    notifies :run, "execute[postmap #{text_map}]", :immediately
+    notifies :run, "execute[postmap #{map_type}:#{text_map}]", :immediately
     notifies :reload, 'service[postfix]'
     variables(
       :db_file => text_map_rel,
     )
   end
 
-  execute "postmap #{text_map}" do
+  execute "postmap #{map_type}:#{text_map}" do
     action :nothing
   end
 end
