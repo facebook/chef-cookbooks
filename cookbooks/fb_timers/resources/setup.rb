c788f755cffa92156fd0cb7e0077967842331c89
Cooper Lees <cooper@meta.com>
2024-03-11 05:38:40 -0700

    Shard out disabling `requires_enable` API (25%)
    Differential Revision: D54608083
    
    fbshipit-source-id: 9fd6ae63183a5ff1b0c5673c2f5a5a35bc9b95e5

--- cookbooks/fb_timers/resources/setup.rb
+++ cookbooks/fb_timers/resources/setup.rb
@@ -78,8 +78,8 @@ action :run do
     # and to deprecate the API parameter once done
     # Check it's not already disabled elsewhere
     if conf['requires_enable'] == true
-      # Work down shard - set false for any box in shard 89-99 (10%)
-      conf['requires_enable'] = node.in_shard?(89)
+      # Work down shard - set false for any box in shard 74-99 (25%)
+      conf['requires_enable'] = node.in_shard?(74)
     end
 
     node.default['fb_timers']['jobs'][name] = conf
