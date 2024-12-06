53432dc5d4cc40493b1e0926d147e509638763da
Phil Dibowitz <phil@ipom.com>
2024-12-06 08:25:55 -0800

    new cookbook: fb_smartmon (#203)
    Summary:
    This is a very simple cookbook to install and configure smartmontools.
    
    This was primarily written by infcted
    
    Pull Request resolved: https://github.com/facebook/chef-cookbooks/pull/203
    
    Test Plan: Potentially usable internally (we don't yet have a dedicated smartmon cookbook beyond engenv_smartd FWICT, but for now this would be an example on our open-source repo and not in use.
    
    Differential Revision: D66516318
    
    fbshipit-source-id: d1742fa753665cfdecd317354f0d69cd7f8d5005

---
+++ cookbooks/fb_smartmon/metadata.rb
@@ -0,0 +1,11 @@
+# Copyright (c) 2022-present, Viarious, Inc.
+name 'fb_smartmon'
+maintainer 'Facebook'
+maintainer_email 'noreply@facebook.com'
+license 'All Rights Reserved'
+source_url 'https://github.com/facebook/chef-cookbooks/'
+description 'Installs/Configures fb_smartmon'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+# never EVER change this number, ever.
+version '0.1.0'
+depends 'fb_helpers'
