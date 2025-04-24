facebook/chef-cookbooks

commit 68f51c1e8347fd543c9253802015f90509f7953b

fb_init_sample: minor updates
Summary: Add fb_dnf, clean up some C6 stuff

Differential Revision: D73541337

fbshipit-source-id: b2669426383f7c42f19285a38a5c8c0a68238a64

cookbooks/fb_init_sample/recipes/default.rb
@@ -30,7 +30,7 @@ end
 include_recipe 'fb_init_sample::site_settings'
 
 if node.centos?
-  # HERE: yum
+  include_recipe 'fb_dnf'
   include_recipe 'fb_rpm'
 end
 if node.debian? || node.ubuntu?
@@ -132,18 +132,18 @@ if node.firstboot_tier?
   include_recipe 'fb_init_sample::firstboot'
 end
 
-unless node.centos6?
-  # not packaged in C10 and above
+if node.centos?
+  # https://bugzilla.redhat.com/show_bug.cgi?id=2337139
   if node.centos_max_version?(9)
     include_recipe 'fb_apcupsd'
   end
   # Turn off dnsmasq as it doesn't play well with travis
   node.default['fb_dnsmasq']['enable'] = false
   include_recipe 'fb_dnsmasq'
-end
-# not packaged in C10 and above
-unless node.centos10?
-  include_recipe 'fb_collectd'
+  # https://bugzilla.redhat.com/show_bug.cgi?id=2345748
+  if node.centos_max_version?(9)
+    include_recipe 'fb_collectd'
+  end
 end
 include_recipe 'fb_rsync::server'
 if node.centos?

cookbooks/fb_init_sample/metadata.rb
@@ -18,6 +18,7 @@ depends 'fb_apt'
 depends 'fb_chrony'
 depends 'fb_collectd'
 depends 'fb_cron'
+depends 'fb_dnf'
 depends 'fb_dnsmasq'
 depends 'fb_dracut'
 depends 'fb_e2fsprogs'

commit 884a3305bdc3e771c452b975e3198c185bf632a5

Fix fb_init_sample
Commit 68f51c1 accidentally removed a bunch of stuff from Ubuntu and
Debian. This fixes the logic to put it back.

That fixes current test failures.

Signed-off-by: Phil Dibowitz <phil@ipom.com>

cookbooks/fb_init_sample/recipes/default.rb
@@ -134,18 +134,20 @@ if node.firstboot_tier?
   include_recipe 'fb_init_sample::firstboot'
 end
 
-if node.centos?
-  # https://bugzilla.redhat.com/show_bug.cgi?id=2337139
-  if node.centos_max_version?(9)
-    include_recipe 'fb_apcupsd'
-  end
-  # Turn off dnsmasq as it doesn't play well with travis
-  node.default['fb_dnsmasq']['enable'] = false
-  include_recipe 'fb_dnsmasq'
-  # https://bugzilla.redhat.com/show_bug.cgi?id=2345748
-  if node.centos_max_version?(9)
-    include_recipe 'fb_collectd'
-  end
+# APCUPSD is not yet in C10+
+# https://bugzilla.redhat.com/show_bug.cgi?id=2337139
+if !node.rhel_family? || node.el_max_version?(9)
+  include_recipe 'fb_apcupsd'
+end
+
+# Turn off dnsmasq as it doesn't play well with travis
+node.default['fb_dnsmasq']['enable'] = false
+include_recipe 'fb_dnsmasq'
+
+# Collected is not yet in C10+
+# https://bugzilla.redhat.com/show_bug.cgi?id=2345748
+if !node.rhel_family? || node.el_max_version?(9)
+  include_recipe 'fb_collectd'
 end
 include_recipe 'fb_rsync::server'
 if node.centos?