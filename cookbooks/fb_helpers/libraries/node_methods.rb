98beeda912b371dee1d1afa1fdfaee159db69b44
Ryan Yee <ryee@meta.com>
2025-08-05 09:56:02 -0700

    Update for RHEL 10
    Differential Revision: D79613622
    
    fbshipit-source-id: 6ca6c353ec9602a90ec69ebc20141557727324ad

--- cookbooks/fb_helpers/libraries/node_methods.rb
+++ cookbooks/fb_helpers/libraries/node_methods.rb
@@ -1104,7 +1104,7 @@ class Chef
     # returns the version-release of an rpm installed, or nil if not present
     def rpm_version(name)
       if (self.centos? && !self.centos7?) || self.fedora? || self.redhat8? || self.oracle8? || self.redhat9? ||
-        self.oracle9? || self.aristaeos_4_30_or_newer?
+        self.oracle9? || self.redhat10? || self.aristaeos_4_30_or_newer?
         # returns epoch.version
         v = Chef::Provider::Package::Dnf::PythonHelper.instance.
             package_query(:whatinstalled, name).version
