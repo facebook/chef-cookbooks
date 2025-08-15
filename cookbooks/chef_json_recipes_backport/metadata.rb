c59cffbbc840d1a54081dfff49147d63f54215db
David Crosby <dcrosby@meta.com>
2025-08-15 10:18:28 -0700

    Add support for JSON recipes
    Summary:
    I expect we'll be wanting to start using JSON recipes in our
    open-source cookbooks pretty soon. This means having a path for
    downstream users to either avoid using JSON recipes right away (we can keep the
    old Ruby recipes for some period of time, gated by
    `node.json_recipes_supported?`), or use JSON recipes on older clients by
    including the `chef_json_recipes_backport` cookbook within their cookbooks.
    While this patch is minimally invasive affecting one method, that method is
    `load_recipe`, so that should be an opt-in effort for some amount of time
    (maybe once Chef 19 has been on public release for a while).
    
    Differential Revision: D79760668

---
+++ cookbooks/chef_json_recipes_backport/metadata.rb
@@ -0,0 +1,8 @@
+# Copyright (c) 2025-present, Meta Platforms, Inc.
+name 'chef_json_recipes_backport'
+maintainer 'Facebook'
+maintainer_email 'noreply@facebook.com'
+license 'Apache-2.0'
+description 'Monkeypatch to enable JSON recipes in Chef 16.18.0 and greater'
+source_url 'https://github.com/facebook/chef-cookbooks/'
+version '0.0.1'
