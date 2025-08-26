f59aa29bbe52879221d72b286605c6de73d669f0
Ryan Yee <ryee@meta.com>
2025-08-26 09:06:48 -0700

    Add OL10 dependencies
    Differential Revision: D80970892
    
    fbshipit-source-id: c226bdfede83e5d031e426e63a5efb2182e4263e

--- cookbooks/fb_helpers/libraries/node_methods.rb
+++ cookbooks/fb_helpers/libraries/node_methods.rb
@@ -1251,7 +1251,7 @@ class Chef
     def rpm_version(name)
       if (self.centos? && !self.centos7?) || self.fedora? || self.redhat8? ||
           self.oracle8? || self.redhat9? || self.oracle9? || self.redhat10? ||
-          self.aristaeos_4_30_or_newer?
+          self.oracle10? || self.aristaeos_4_30_or_newer?
         # returns epoch.version
         v = Chef::Provider::Package::Dnf::PythonHelper.instance.
             package_query(:whatinstalled, name).version
