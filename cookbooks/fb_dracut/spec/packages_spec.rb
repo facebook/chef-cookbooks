8208348bd98a386793c44c503a3dc15b8dae51be
David Crosby <dcrosby@meta.com>
2025-12-09 08:23:37 -0800

    Add ChefSpec tests for packages recipe
    Summary:
    Add spec coverage for the packages recipe which manages the dracut
    package installation. Tests verify the package upgrade behavior based
    on both manage_packages and disable attributes.
    
    Reviewed By: AadityaNair
    
    Differential Revision: D88485339
    
    fbshipit-source-id: e9e42e0030ff3859553256ebd41c411bc35e26f2

---
+++ cookbooks/fb_dracut/spec/packages_spec.rb
@@ -0,0 +1,53 @@
+# Copyright (c) 2025-present, Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require './spec/spec_helper'
+
+recipe 'fb_dracut::packages', :unsupported => [:mac_os_x] do |tc|
+  let(:chef_run) do
+    tc.chef_run
+  end
+
+  context 'when manage_packages is false' do
+    it 'does not upgrade the dracut package' do
+      chef_run.converge(described_recipe) do |node|
+        node.default['fb_dracut']['manage_packages'] = false
+      end
+      expect(chef_run).not_to upgrade_package('dracut')
+    end
+  end
+
+  context 'when manage_packages is true' do
+    context 'when disable is true' do
+      it 'does not upgrade the dracut package' do
+        chef_run.converge(described_recipe) do |node|
+          node.default['fb_dracut']['manage_packages'] = true
+          node.default['fb_dracut']['disable'] = true
+        end
+        expect(chef_run).not_to upgrade_package('dracut')
+      end
+    end
+
+    context 'when disable is false' do
+      it 'upgrades the dracut package' do
+        chef_run.converge(described_recipe) do |node|
+          node.default['fb_dracut']['manage_packages'] = true
+          node.default['fb_dracut']['disable'] = false
+        end
+        expect(chef_run).to upgrade_package('dracut')
+      end
+    end
+  end
+end
